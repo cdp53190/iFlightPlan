4a5c03b6cd0a28da021aad01dfb0bcd534799a0e
光田 征史 <micchann@m2.pbc.ne.jp>
2016-03-23 15:18:01 -0700

    Initial Commit

---
+++ iFlightPlan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iFlightPlan
+//
+//  Created by Seiji Mitsuda on 2016/03/23.
+//  Copyright © 2016年 Another Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iFlightPlan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iFlightPlan
+//
+//  Created by Seiji Mitsuda on 2016/03/23.
+//  Copyright © 2016年 Another Sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
